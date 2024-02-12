703488742363a9ccb2b56084948c5d0052dbb323
saketh <sakethsid@gmail.com>
2024-02-12 18:09:17 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import React from 'react';
+import { BrowserRouter as Router,Routes, Route } from 'react-router-dom';
+import Login from './components/Auth/Login';
+import Register from './components/Auth/Register';
+import RoomList from './components/Room/RoomList';
+import BookingForm from './components/Room/BookingForm';
+import Dashboard from './components/User/Dashboard';
+import BookingDetails from './components/User/BookingDetails';
+import AdminDashboard from './components/Admin/Dashboard';
+import BookingList from './components/Admin/BookingList';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+     <Routes>
+     <div>
+        {/* Define routes */}
+        <Route path="/auth/login" component={Login} />
+        <Route path="/auth/register" component={Register} />
+        <Route path="/room/list" component={RoomList} />
+        <Route path="/room/book" component={BookingForm} />
+        <Route path="/user/dashboard" component={Dashboard} />
+        <Route path="/user/booking-details" component={BookingDetails} />
+        <Route path="/admin/dashboard" component={AdminDashboard} />
+        <Route path="/admin/booking-list" component={BookingList} />
+      </div>
+     </Routes>
+    </Router>
   );
 }
 
